1d8042eb0779951cf13f280d0d4b49a501981de3
Lucas Gorges <lucasgorges@hotmail.com>
2023-02-15 14:50:27 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+var user = {
+  username: "",
+  password: "",
+};
+
+var usuarioCorreto = {
+  username: "admin",
+  senha: "123456",
+};
+if (localStorage.getItem("username") && localStorage.getItem("password")) {
+  user.username = localStorage.getItem("username");
+  user.password = localStorage.getItem("password");
+  username.value = user.username;
+  password.value = user.password;
+
+  if (
+    user.username === usuarioCorreto.username &&
+    user.password === usuarioCorreto.senha
+  ) {
+    window.location.href = "logout.html";
+  }
+}
+
+function clicou() {
+  if (
+    user.username === usuarioCorreto.username &&
+    user.password === usuarioCorreto.senha
+  ) {
+    localStorage.setItem("username", user.username);
+    localStorage.setItem("password", user.password);
+
+    window.location.href = "logout.html";
+  } else {
+    alert("Usuário ou senha incorretos.");
+  }
+}
+
+let login = document.getElementById("login");
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+
+const handleChangeUsername = (event) => {
+  user.username = event.target.value;
+};
+const handleChangePassword = (event) => {
+  user.password = event.target.value;
+};
+
+login.addEventListener("click", clicou);
+username.addEventListener("change", handleChangeUsername);
+password.addEventListener("change", handleChangePassword);
+
+if (localStorage.getItem("username") && localStorage.getItem("password")) {
+  user.username = localStorage.getItem("username");
+  user.password = localStorage.getItem("password");
+  username.value = user.username;
+  password.value = user.password;
+}
+
+function clicou() {
+  if (
+    user.username === usuarioCorreto.username &&
+    user.password === usuarioCorreto.senha
+  ) {
+    localStorage.setItem("username", user.username);
+    localStorage.setItem("password", user.password);
+    alert("Você está logado");
+  } else {
+    alert("Usuário ou senha incorretos.");
+  }
+  document.getElementById("logout").addEventListener("click", function () {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+
+    window.location.href = "index.html";
+  });
+}
